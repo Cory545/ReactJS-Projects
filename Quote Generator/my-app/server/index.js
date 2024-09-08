8bebfd09038a2a2504d48a923d753cb718aef2fd
Cory545 <coryburton545@gmail.com>
2024-09-08 15:42:58 +0100

    Finish basic functionality

--- Quote Generator/my-app/server/index.js
+++ Quote Generator/my-app/server/index.js
@@ -2,10 +2,23 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get('/api/data', (req, res) => {
-  // Handle your API logic here
-  const data = { message: 'Hello from the server!' };
-  res.json(data);
+app.get('/api/data', async (req, res) => {
+  try {
+    //Make the url a variable, and then fetch() it
+    const url = "https://api.quotable.io/quotes/random";
+    const response = await fetch(url);
+    //If response is not ok - error.
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    // Get the response in json.
+    const json = await response.json();
+    // Grab the first one in the array of the json variable.
+    const quote = json[0];
+    res.status(200).json(quote); // Send the quote object as JSON response
+  } catch (error) {
+    console.log(error);
+  };
 });
 
 app.listen(port, () => {
--- Quote Generator/my-app/src/App.js
+++ Quote Generator/my-app/src/App.js
@@ -1,25 +1,34 @@
 import './App.css';
-
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
   const [data, setData] = useState('');
 
+    //When fetchQuote is called - it will then do a GET request to "/api/data" which is handeled serverside.
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get('/api/data');
+      //Although not in a React Component as it sits now, but it will be when its called by useEffect
+      setData(response.data.content); // Update state with new quote
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Fetch data on initial render and button click
   useEffect(() => {
-    axios.get('/api/data')
-      .then(response => {
-        setData(response.data.message);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    fetchQuote();
   }, []);
 
   return (
     <div className="App">
-      <h1>React and Node.js Integration</h1>
-      <p>Message from the server: {data}</p>
+      <h1>Get a Random Quote!</h1>
+      <hr />
+      <p>Random Quote:</p>
+      <button onClick={fetchQuote}>Get New Quote</button>
+      <hr />
+      <p>"{data}"</p>
     </div>
   );
 }
